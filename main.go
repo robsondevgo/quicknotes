5364cfc13d3f15d90ace312042746d2f70271262
Robson Gomes <nosbor84@gmail.com>
2023-12-25 14:26:41 -0300

    handle func

--- main.go
+++ main.go
@@ -5,26 +5,14 @@ import (
 	"net/http"
 )
 
-type WorldHandler struct{}
-
-func (WorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("World"))
-}
-
-type HelloHandler struct{}
-
-func (HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello"))
 }
 
 func main() {
 	fmt.Println("Servidor rodando na porta 5000")
 
-	world := WorldHandler{}
-	hello := HelloHandler{}
+	// http.Handle("/hello", hello)
 
-	http.Handle("/hello", hello)
-	http.Handle("/world", world)
-
-	http.ListenAndServe(":5000", nil)
+	http.ListenAndServe(":5000", http.HandlerFunc(helloHandler))
 }
