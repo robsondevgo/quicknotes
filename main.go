15f7e6eaef119f39b2891a0486a8ec1b178da5d5
Robson Gomes <nosbor84@gmail.com>
2023-12-27 16:01:30 -0300

    novas rotas e manipulação de headers

--- main.go
+++ main.go
@@ -5,29 +5,41 @@ import (
 	"net/http"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Home Handler")
+func noteList(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Header().Add("Teste", "123")
+
+	w.Header()["Date"] = nil //suprimir esse cabeçalho
+
+	w.Header().Del("Teste")
+
+	fmt.Fprint(w, `{"id": 1}`)
 }
 
-func olaHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Ola Handler")
+func noteView(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Exibindo uma nota...")
+}
+
+func noteCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+
+		w.Header().Set("Allow", http.MethodPost)
+
+		//rejeitar a requisição
+		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprint(w, "Criando uma nova nota...")
 }
 
 func main() {
 	fmt.Println("Servidor rodando na porta 5000")
 	mux := http.NewServeMux()
-	mux.HandleFunc("/ola/", olaHandler)
-	mux.HandleFunc("/ola/mundo", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Olá, mundo Handler")
-	})
-	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World Handler")
-	})
-	mux.HandleFunc("/", homeHandler)
 
-	mux.HandleFunc("ola.pessoas.com/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Este é o meu site de olá")
-	})
+	mux.HandleFunc("/", noteList)
+	mux.HandleFunc("/note/view", noteView)
+	mux.HandleFunc("/note/create", noteCreate)
 
 	http.ListenAndServe(":5000", mux)
 }
