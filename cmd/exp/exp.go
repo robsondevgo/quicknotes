acd31ba980a2cd5c37f2c04839585c19e92ce4d2
Robson Gomes <nosbor84@gmail.com>
2024-01-06 13:50:12 -0300

    env variables

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -2,27 +2,19 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
-type TemplateData struct {
-	Nome string
-	Age  int
-}
-
 func main() {
-	t, err := template.ParseFiles("layout1.html", "footer.html", "header.html")
-
-	fmt.Println(t.Name())
-
-	fmt.Println(t.DefinedTemplates())
-
+	err := godotenv.Load()
 	if err != nil {
 		panic(err)
 	}
-	err = t.ExecuteTemplate(os.Stdout, "layout1.html", "2023")
-	if err != nil {
-		panic(err)
-	}
+
+	port := os.Getenv("PORT")
+	host := os.Getenv("HOST")
+	fmt.Printf("%s:%s", host, port)
+
 }
