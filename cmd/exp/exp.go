fe842a8ec5a4ea3ca0cd4b8eefa4633436e8e66b
Robson Gomes <nosbor84@gmail.com>
2024-01-19 18:55:02 -0300

    go error handling

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,21 +1,52 @@
 package main
 
 import (
-	"log/slog"
-	"os"
+	"errors"
+	"fmt"
 )
 
+var CustomErr100 = &CustomError{msg: "aconteceu um erro 100", code: 100}
+var CustomErr200 = &CustomError{msg: "aconteceu um erro 200", code: 200}
+
+type CustomError struct {
+	msg  string
+	code int
+}
+
+func NewCustomError(msg string, code int) error {
+	return &CustomError{msg: msg, code: code}
+}
+
+func (ce *CustomError) Error() string {
+	return ce.msg
+}
+
+func subOperation() error {
+	return CustomErr100
+}
+
+func execute() error {
+	err := subOperation()
+	if err != nil {
+		return fmt.Errorf("%s: (%w)", err, err)
+	}
+	return nil
+}
+
+// wrap e unrap
 func main() {
-	// h := slog.NewTextHandler(os.Stderr, nil)
-	h := slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
-		Level:     slog.LevelDebug,
-		AddSource: true,
-	})
-
-	log := slog.New(h).With("app", "exp")
-
-	log.Debug("debug message")
-	log.Info("info message", slog.Group("request_info", "request_id", 1, "user", "robson"))
-	log.Warn("warn message")
-	log.Error("error message", "request_id", 1)
+	err := execute()
+	if err != nil {
+		fmt.Println(err)
+		err = errors.Unwrap(err)
+		fmt.Println(err)
+		err = errors.Unwrap(err)
+		fmt.Println(err)
+	}
+	// var err100 *CustomError
+	// if errors.As(err, &err100) {
+	// 	fmt.Println(err100.code)
+	// } else {
+	// 	fmt.Println(err)
+	// }
 }
