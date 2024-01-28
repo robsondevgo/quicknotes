20265f8186a19b455056f4aedad91f924484d333
Robson Gomes <nosbor84@gmail.com>
2024-01-28 09:55:09 -0300

    interagindo com o banco sql

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,54 +1,131 @@
 package main
 
 import (
-	"errors"
+	"context"
 	"fmt"
-	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
-var Err100 = &CustomError{msg: "não foi possível executar o processo", code: 100}
-
-type CustomError struct {
-	msg  string
-	code int
+type Post struct {
+	id      int
+	title   string
+	content string
+	author  string
 }
 
-func (c *CustomError) Error() string {
-	return fmt.Sprintf("%s %d", c.msg, c.code)
-}
-
-func NewCustomError(msg string, code int) error {
-	return &CustomError{msg: msg, code: code}
-}
-
-func process() (string, error) {
-	f, err := os.Open("foo")
-	if err != nil {
-		// return "", errors.New("não foi possível executar o processo")
-		// return "", fmt.Errorf("não foi possível executar o processo")
-		// return "", Err100
-		return "", fmt.Errorf("não foi possível executar o processo: (%w)", err)
-	}
-	return f.Name(), nil
-}
+var conn *pgx.Conn
 
 func main() {
-	//deu certo a leitura
-	r, err := process()
+	var err error
+	dbURL := "postgres://postgres:secret@localhost:5432/postgres"
+	conn, err = pgx.Connect(context.Background(), dbURL)
 	if err != nil {
-		// var err100 *CustomError
-		// if errors.As(err, &err100) {
-		// 	fmt.Println(err100.code)
-		// }
-		// return
-		fmt.Println(err)
-		err = errors.Unwrap(err)
-		fmt.Println(err)
-		fmt.Println(errors.Unwrap(err))
-		err = errors.Unwrap(err)
-		fmt.Println(errors.Unwrap(err))
+		panic(err)
+	}
+	fmt.Println("Conexão com o banco foi efetuada com sucesso")
+	defer conn.Close(context.Background())
 
+	// createTable()
+	// insertPost()
+	// insertPostWithReturn()
+	// selectPostById()
+	selectAllPosts()
+}
+
+func createTable() {
+	query := `
+		CREATE TABLE IF NOT EXISTS posts (
+			id SERIAL PRIMARY KEY,
+			title TEXT NOT NULL,
+			content TEXT, 
+			author TEXT NOT NULL
+		);
+	`
+	_, err := conn.Exec(context.Background(), query)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Table posts created")
+}
+
+// SQL INJECTION
+func insertPost() {
+	title := "','',''); DROP TABLE posts; --"
+	content := "Conteúdo do post 1"
+	author := "robson"
+	// query := fmt.Sprintf(`
+	// 	INSERT INTO posts (title, content, author)
+	// 	values ('%s', '%s', '%s')
+	// `, title, content, author)
+
+	query := `
+		INSERT INTO posts (title, content, author)
+		values ($1, $2, $3)
+	`
+	//fmt.Println(query)
+	_, err := conn.Exec(context.Background(), query, title, content, author)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Post criado com sucesso")
+}
+
+func insertPostWithReturn() {
+	title := "Post 3"
+	content := "Conteúdo do post 3"
+	author := "robson"
+	query := `
+		INSERT INTO posts (title, content, author)
+		values ($1, $2, $3) RETURNING id;
+	`
+	row := conn.QueryRow(context.Background(), query, title, content, author)
+	var id int
+	if err := row.Scan(&id); err != nil {
+		panic(err)
+	}
+	fmt.Println("Post criado. Id =", id)
+}
+
+func selectPostById() {
+	id := 4
+	var title, content, author string
+	query := "select title, content, author from posts where id = $1"
+	row := conn.QueryRow(context.Background(), query, id)
+	err := row.Scan(&title, &content, &author)
+	if err == pgx.ErrNoRows {
+		fmt.Println("No post found for id =", id)
 		return
 	}
-	fmt.Println(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Post: title=%s, content=%s, author=%s \n", title, content, author)
+}
+
+func selectAllPosts() {
+	query := "select id, title, content, author from posts"
+	// query := "select * from posts"
+	rows, err := conn.Query(context.Background(), query)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	if rows.Err() != nil {
+		panic(rows.Err())
+	}
+
+	var posts []Post
+	for rows.Next() {
+		var post Post
+		err = rows.Scan(&post.id, &post.title, &post.content, &post.author)
+		if err != nil {
+			panic(err)
+		}
+		posts = append(posts, post)
+	}
+	for _, post := range posts {
+		fmt.Println(post)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,11 @@ module github.com/robsondevgo/quicknotes
 go 1.21.5
 
 require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.2 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
